3fba0953ed1eff16b98104b3a7d082dd0a42d58b
Brian Akins <bakins@omniti.com>
2014-08-12 13:37:10 -0400

    correct struct name

---
+++ pprof.go
@@ -0,0 +1,46 @@
+package pprof
+
+import (
+	"net/http"
+	"net/http/pprof"
+	runtime_pprof "runtime/pprof"
+	"strings"
+)
+
+type handler struct {
+}
+
+var profileHandlers map[string]http.Handler
+
+func init() {
+	profileHandlers = make(map[string]http.Handler)
+	for _, profile := range runtime_pprof.Profiles() {
+		profileHandlers[profile.Name()] = pprof.Handler(profile.Name())
+	}
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	// horrible
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 3 || len(parts) > 4 || parts[1] != "debug" || parts[2] != "pprof" {
+		next(w, r)
+		return
+	}
+
+	if len(parts) == 3 || (len(parts) == 4 && parts[3] == "") {
+		pprof.Index(w, r)
+		return
+	}
+
+	handler := profileHandlers[parts[3]]
+	if handler == nil {
+		next(w, r)
+		return
+	}
+	handler.ServeHTTP(w, r)
+	return
+}
+
+func Pprof() *handler {
+	return &handler{}
+}
